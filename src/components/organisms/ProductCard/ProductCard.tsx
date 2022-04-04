b0d81cec5bd44a449b19af5f3e696fa8b3e1c9bf
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-04-04 19:12:29 +0530

    feat: craeted skewed carousel

--- src/components/organisms/ProductCard/ProductCard.tsx
+++ src/components/organisms/ProductCard/ProductCard.tsx
@@ -13,22 +13,31 @@ const ProductCard = React.forwardRef(
   ({ src, title, children }: IProductCardProps, ref) => (
     <div className='grid h-screen grid-cols-2 gap-6 py-6' ref={ref}>
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <div className='flex gap-2 overflow-x-scroll bg-white '>
-        <div className='flex-shrink-0 -skew-x-3 '>
+      <div className='flex gap-2 overflow-x-scroll -skew-x-3 bg-white'>
+        <div className='relative flex-shrink-0'>
           <img
             src='https://res.cloudinary.com/thankyou/image/upload/v1649076995/iamkarthick/46aP2QbqUqBqpx5c7Zikd2HFMqywJNqDeZVr22vYLLq3mmDMvC912rHdNBhAedaBaUAW5aF7NFVhWLBXM7zWakKcqK41_vb0mox.gif'
             alt='pic'
-            className='absolute object-cover h-full w-72 -z-10'
+            className='object-cover h-full bg-fixed w-72 -z-10'
           />
+          <div className='absolute bottom-0 w-full p-4 text-white bg-black/60'>
+            <div className='skew-x-3'>
+              <div className='text-3xl font-bold'>Hello</div>
+              <div className='mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+                cupiditate asperiores pariatur!
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='flex-shrink-0 -skew-x-3'>
+        <div className='flex-shrink-0 '>
           <img
             src='https://res.cloudinary.com/thankyou/image/upload/v1649076995/iamkarthick/46aP2QbqUqBqpx5c7Zikd2HFMqywJNqDeZVr22vYLLq3mmDMvC912rHdNBhAedaBaUAW5aF7NFVhWLBXM7zWakKcqK41_vb0mox.gif'
             alt='pic'
             className='object-cover h-full w-72'
           />
         </div>
-        <div className='flex-shrink-0 -skew-x-3'>
+        <div className='flex-shrink-0 '>
           <img
             src='https://res.cloudinary.com/thankyou/image/upload/v1649076995/iamkarthick/46aP2QbqUqBqpx5c7Zikd2HFMqywJNqDeZVr22vYLLq3mmDMvC912rHdNBhAedaBaUAW5aF7NFVhWLBXM7zWakKcqK41_vb0mox.gif'
             alt='pic'
