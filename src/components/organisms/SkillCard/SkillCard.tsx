977d5562f8277869b88ac7657df2ee6664506f3d
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-04-05 11:04:00 +0530

    fix: updated break inside within skill card

--- src/components/organisms/SkillCard/SkillCard.tsx
+++ src/components/organisms/SkillCard/SkillCard.tsx
@@ -8,29 +8,38 @@ export interface ISkillCardProps {
   type?: 'title' | 'subtitle'
 }
 
+//  className={`${type === 'title' && 'break-before-column'}`}
+
 const SkillCard = ({
   title,
   children,
   experience,
   type = 'subtitle',
 }: ISkillCardProps) => (
-  <div className={`${type === 'title' && 'break-before-column'}`}>
-    <div className='bg-white-transparent'>
-      <div
-        className={`inline-block ${
-          type === 'title' && 'text-5xl text-luxury'
-        } ${type === 'subtitle' && 'text-2xl'} font-black bg-white-transparent`}
-      >
-        {title}
-      </div>
-    </div>
-    {experience && (
-      <div className='bg-white-transparent'>
-        {experience} {experience === 1 ? 'year' : 'years'}
-      </div>
+  <>
+    {type === 'title' && (
+      <div className={`${type === 'title' && 'break-before-column'}`} />
     )}
-    <div className='space-y-4 text-lg bg-white-transparent'>{children}</div>
-  </div>
+    <div className='break-inside-avoid'>
+      <div className='bg-white-transparent '>
+        <div
+          className={`inline-block ${
+            type === 'title' && 'text-5xl text-luxury'
+          } ${
+            type === 'subtitle' && 'text-2xl'
+          } font-black bg-white-transparent`}
+        >
+          {title}
+        </div>
+      </div>
+      {experience && (
+        <div className='bg-white-transparent'>
+          {experience} {experience === 1 ? 'year' : 'years'}
+        </div>
+      )}
+      <div className='space-y-4 text-lg bg-white-transparent'>{children}</div>
+    </div>
+  </>
 )
 
 export default SkillCard
